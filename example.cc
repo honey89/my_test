9dc23cc18c19458fc028dd90ae5cf9872baa1a49
honey <honeyyan@zego.im>
2016-05-04 11:16:03 +0800

    demo 2016.5.3

---
+++ example.cc
@@ -0,0 +1,55 @@
+//
+//  main.cpp
+//  app_auth
+//
+//  Created by honey on 16/4/27.
+//  Copyright © 2016年 zego. All rights reserved.
+//
+#include<stdio.h>
+#include<omp.h>
+#include<string.h>
+#include<time.h>
+#include<stdlib.h>
+#include<iostream>
+using namespace std;
+
+
+
+#define NMAX 4096 * 10
+#define NUM_CORE 4
+
+int g_iBuff[NMAX];
+
+
+int main()
+{
+    int alignPos = 0;
+    for (int i = 0; i < 65; ++i)
+    {
+        if (((int)&g_iBuff[i]) % 64 == 0)
+        {
+            alignPos = i;
+            break;
+        }
+    }
+    
+    for (int pos = 1; pos <= 4096; pos <<= 1)
+    {
+        memset(g_iBuff, 0 , sizeof(g_iBuff));
+        time_t beg = time(0);
+#pragma omp parallel for num_threads(NUM_CORE)
+        for (int i = 0; i < NUM_CORE; ++i)
+        {
+            int tp = i * pos + alignPos;
+            for (int j = 0; j < 999999999; ++j)
+            {
+                g_iBuff[tp]++;
+            }
+        }
+        time_t end = time(0);
+        
+        cout << "false shareing: step = "<<pos<<" time = "<< end-beg << "s" <<endl;
+    }
+    
+    return 0;
+}
---
+++ udp_client.h
@@ -0,0 +1,14 @@
+//
+//  udp_client.h
+//  
+//
+//  Created by honey on 16/4/7.
+//
+//
+
+#ifndef udp_client_h
+#define udp_client_h
+
+#include <stdio.h>
+
+#endif /* udp_client_h */
